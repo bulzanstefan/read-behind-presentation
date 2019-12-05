0ef9b525590f595859184281e0e8596f8140bd70
Bulzan Stefan <stefan.bulzan@nordlogic.com>
2019-12-05 11:14:22 +0200

    added default name

--- read-behind-slow-producer/src/main/java/com/javaadvent/readbehind/producer/ProducerController.java
+++ read-behind-slow-producer/src/main/java/com/javaadvent/readbehind/producer/ProducerController.java
@@ -1,6 +1,9 @@
 package com.javaadvent.readbehind.producer;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -9,7 +12,7 @@ import java.util.Date;
 @RequestMapping("producer")
 public class ProducerController {
     @GetMapping
-    String produce(@RequestParam String name) throws InterruptedException {
+    String produce(@RequestParam(required = false, defaultValue = "default") String name) throws InterruptedException {
         Thread.sleep(5000);
         return name + " : " + new SimpleDateFormat("HH:mm:ss.SSS").format(new Date());
     }
